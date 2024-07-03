f0b28c812e29b7d79c482b8c01d6367010ee97f8
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-07-03 23:49:58 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,30 @@
-import express, { Application, Request, Response } from "express";
+import db from "./db"
+import bcrypt from "bcrypt";
+import express from "express";
 import dotenv from "dotenv";
-import db from "./db";
-
-
-const app: Application = express();
+import cors from "cors";
+import bodyParser from "body-parser";
 
 dotenv.config();
+const app = express();
 
-app.use(express.json());
+const allowedOrigins = [
+    "http://localhost:3000",
+  ];
 
-db();
+  
+  app.use(cors({ credentials: true, origin: allowedOrigins }));
+  app.use(bodyParser.json());
 
-// Default
-app.get("/api", (req: Request, res: Response) => {
-  res.status(201).json({ message: "Welcome to Auth ts" });
-});
 
-const PORT = process.env.PORT || 5000;
+  
+  db();
 
-app.listen(PORT, (): void => console.log(`Server is running on ${PORT}`));
+  app.listen(process.env.PORT || 5030, () => {
+    console.log(`Server is running on port ${process.env.PORT || 5030}`);
+  } );
+
+  app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+

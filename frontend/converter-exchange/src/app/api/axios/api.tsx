1c4d4dfaa1ddf794957f732a031024d97a8437c4
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-07-10 12:58:44 +0530

    Update api.tsx

--- frontend/converter-exchange/src/app/api/axios/api.tsx
+++ frontend/converter-exchange/src/app/api/axios/api.tsx
@@ -2,12 +2,10 @@
 import axios from "axios";
 //Api Client
 const apiClient = axios.create({
-  baseURL: "http://localhost:5050",
+  baseURL: "https://tecsota-assessment.vercel.app",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
 });
 export { apiClient };
-
-//https://tecsota-assessment.vercel.app
